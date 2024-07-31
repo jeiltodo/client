cd903dbbda9f5c5bda2c5d6d2bcf8cfb135fcf7a
dragonite-lee <ross1222@naver.com>
2024-07-31 15:09:33 +0900

    #9 feat: sidebar 기능 구현완료 구성요소 제작 중

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,10 +1,11 @@
+
 import React from 'react';
-import { Sidebar } from '@jeiltodo/ui';
+
 
 export default function Page() {
   return (
-    <main className='bg-gray-100'>
-      <Sidebar>사이드바~</Sidebar>
+    <main className=' p-4 '>
+      ㅇㅇ
     </main>
   );
 }
