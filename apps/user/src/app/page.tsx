6b92146aae626bcd4cf9594fb60e0378bd1853c8
dragonite-lee <ross1222@naver.com>
2024-08-06 15:25:31 +0900

    fix: merge conflicts resolved

--- apps/user/src/app/page.tsx
+++ apps/user/src/app/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ProgressBoard } from '../widgets/user/ui/user-progress-board';
+import { ProgressBoard } from '../widgets/user/ui/progress-board';
 
 export default function Page() {
   return (
