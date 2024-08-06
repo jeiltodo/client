a2f0fb476a4ef8628689a5e0a36f5cbca15704ac
aajy <aajy.000@gmail.com>
2024-08-06 18:02:20 +0900

    Feat: 노트 모아보기 페이지의 page.tsx와 노트 에티터 페이지의 page.tsx 생성
    Related to:#31

---
+++ apps/user/src/app/[goalid]/[todoid]/note/[noteid]/page.tsx
@@ -0,0 +1,3 @@
+export default function NoteEditor() {
+  return <div>노트 편집 페이지 입니다.</div>;
+}
---
+++ apps/user/src/app/[goalid]/note/list/page.tsx
@@ -0,0 +1,3 @@
+export default function NoteList() {
+  return <div>노트 목록을 볼 수 있는 노트 모아보기 페이지 입니다.</div>;
+}
