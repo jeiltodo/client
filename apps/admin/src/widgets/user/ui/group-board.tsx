e435a38963e4c50e0e4ca37de3de86bee8a6054d
dragonite-lee <ross1222@naver.com>
2024-08-06 14:11:00 +0900

    fix: magic number 변경

--- apps/admin/src/widgets/user/ui/group-board.tsx
+++ apps/admin/src/widgets/user/ui/group-board.tsx
@@ -7,7 +7,7 @@ interface GroupBoardProps {
 }
 
 export const GroupBoard: React.FC<GroupBoardProps> = ({ groups }) => {
-  const groupTotalCount = 10;
+  const GROUP_TOTAL_COUNT = 10;
   return (
     <div className='bg-white w-[623px] p-6 rounded-xl'>
       <div className='flex items-center justify-start gap-2'>
@@ -15,7 +15,7 @@ export const GroupBoard: React.FC<GroupBoardProps> = ({ groups }) => {
         <h1 className='text-lg font-semibold'>소속 그룹 정보</h1>
         <div className='ml-[14px] py-[2px] px-1 bg-blue-50 text-slate-800 rounded-[4px] text-xs font-medium'>
           {groups.length}/
-          <span className='text-blue-500'>{groupTotalCount}</span>
+          <span className='text-blue-500'>{GROUP_TOTAL_COUNT}</span>
         </div>
       </div>
       <div className={`mt-4 h-[284px] flex flex-col items-center gap-4 overflow-y-scroll scrollbar-hide`}>
