cc6c25cc72d1b93c75cdb643e5b6c82d6cf43c01
dragonite-lee <ross1222@naver.com>
2024-08-08 09:20:26 +0900

    #23 feat: 내 진행 상황 board 제작 완료

--- apps/user/src/app/page.tsx
+++ apps/user/src/app/page.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import { ProgressBoard } from '../widgets/user/ui/progress-board';
 
 export default function Page() {
+  const [percent, setPercent] = useState(74);
+
+  const increasePercent = () => setPercent(prev => Math.min(prev + 0.3, 100));
+  const decreasePercent = () => setPercent(prev => Math.max(prev - 0.3, 0));
   return (
     <main className=' p-4 '>
-      <ProgressBoard />
+      <ProgressBoard completedPercent={percent} />
+      <div className='mt-20 flex gap-5'>
+        <button onClick={increasePercent} className='bg-blue-300 p-5'>증가</button>
+        <button onClick={decreasePercent} className='bg-blue-300 p-5'>감소</button>
+      </div>
     </main>
   );
 }
--- apps/user/src/widgets/user/ui/progress-board.tsx
+++ apps/user/src/widgets/user/ui/progress-board.tsx
@@ -1,37 +1,26 @@
 import { DoughnutGraph } from '@jeiltodo/icons';
+import { DonutChart, PercentDisplay } from '../../../entities/user';
 
-interface ProgressBoardProps {
-  completedPercent?: number;
+interface Props {
+  completedPercent: number;
 }
-export const ProgressBoard: React.FC<ProgressBoardProps> = ({
-  completedPercent = 74,
+
+export const ProgressBoard: React.FC<Props> = ({
+  completedPercent
 }) => {
   return (
-    <section
-      className='relative h-[250px] flex flex-row bg-blue-500 mobile:w-[343px] tablet:w-[306px] desktop:w-[588px] rounded-[12px] p-base
-    transition-all duration-300 ease-in-out'
-    >
-      <div className='absolute flex flex-col gap-y-[16px] text-white text-lg'>
-        <span className='w-[40px] h-[40px] flex items-center justify-center bg-slate-900 rounded-[15px]'>
+    <section className='relative h-[250px] flex flex-row bg-blue-500 mobile:w-[343px] tablet:w-[306px] desktop:w-[588px] rounded-[12px] p-base'>
+      <div className='absolute flex flex-col gap-y-1 text-white text-lg'>
+        <span className='w-[40px] h-[40px] flex items-center justify-center bg-slate-900 rounded-[15px] mb-3'>
           <DoughnutGraph className='w-[17px]' />
         </span>
         <h2>내 진행 상황</h2>
         <span>
-          <em className='text-3xl font-pretendard-semibold not-italic mr-[4px]'>
-            {completedPercent}
-          </em>
-          &#37;
+          <PercentDisplay value={completedPercent} />
         </span>
       </div>
-      <DoughnutGraph className='absolute -right-10 -bottom-20 w-[224px] opacity-10  rotate-45' />
+      <DonutChart percent={completedPercent} />
+      <DoughnutGraph className='absolute z-10 -right-10 -bottom-20 w-[224px] opacity-10  rotate-45' />
     </section>
   );
 };
-// “progress”: {
-//   “completedPerecent” : number,
-//      “members”: {
-//         “name”: string,
-//         “color” string,
-//         “contributionPercent”: number
-//      }
-//  }
