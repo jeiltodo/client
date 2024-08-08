0487958d426227f77f2c9b863b358a7501ded81b
aajy <aajy.000@gmail.com>
2024-08-08 11:40:37 +0900

    Feat: page reset
    Related to:#33

--- apps/user/src/app/page.tsx
+++ apps/user/src/app/page.tsx
@@ -1,27 +1,5 @@
-'use client';
-import { FlyoutMenu } from '@jeiltodo/ui/shared';
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function Page() {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const onEdit = () => {
-    console.log('수정하기');
-    //수정 로직
-    //할일은 todoId를 넣어서 todo-modal toggle
-    //노트는 goalId, todoId, noteId를 넣은 url로 이동
-  };
-  const onDelete = () => {
-    console.log('삭제하기');
-    //필요한 Id넣어서 삭제API로직
-  };
-  return (
-    <main className='p-4 '>
-      <FlyoutMenu
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-        onEdit={onEdit}
-        onDelete={onDelete}
-      />
-    </main>
-  );
+  return <main className='p-4 '></main>;
 }
