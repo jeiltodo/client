33ec5997c0a5b7b01fc19677960e78ead20db3f6
aajy <aajy.000@gmail.com>
2024-08-14 00:24:52 +0900

    Fix: 에러 응답 처리 수정 - client응답 인터셉터 주석 수정,loginApi error응답수정, login error message 수정
    Related to:#5

--- apps/user/src/entities/session/api/loginApi.ts
+++ apps/user/src/entities/session/api/loginApi.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { client, ResponseWith, setCookie } from '../../../shared';
 import { AuthBody, Token } from '../types';
 
@@ -15,10 +16,13 @@ export const loginApi = async (credentials: AuthBody) => {
       setCookie('accessToken', accessToken);
       setCookie('refreshToken', refreshToken);
     }
-
-    return response.data;
+    return Promise.resolve(response.data);
   } catch (error) {
-    console.error('Login API error:', error);
-    throw error;
+    if (axios.isAxiosError(error)) {
+      if (error.response) {
+        return Promise.resolve(error.response.data); // 오류를 포함한 응답 반환
+      }
+    }
+    return Promise.reject(error); // 네트워크 오류 등 처리할 수 없는 오류는 reject
   }
 };
