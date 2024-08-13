cc3f1775e2f9807af8a47a5d007690b216e81742
dragonite-lee <ross1222@naver.com>
2024-08-13 15:58:36 +0900

    fix: merge error 수정

--- apps/user/src/entities/goal/hooks/useProgressAll.ts
+++ apps/user/src/entities/goal/hooks/useProgressAll.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { goalApi } from '../api/individualGoalsApi';
+import { individualGoalsApi } from '../api/individualGoalsApi';
 import { ResponseWith } from '../../../shared';
 import { UserProgress } from '../../../entities/goal';
 import { goalQueryKeys } from './queryKey';
@@ -7,7 +7,7 @@ import { goalQueryKeys } from './queryKey';
 export const useProgressAll = () => {
   return useQuery({
     queryKey: goalQueryKeys.individual.progress(),
-    queryFn: goalApi.getAllProgress,
+    queryFn: individualGoalsApi.getAllProgress,
     select: (data: ResponseWith<UserProgress>) => data.data,
   });
 };
