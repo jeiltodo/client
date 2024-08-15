c00a96df888f92229cf8349f897d0827af623326
aajy <aajy.000@gmail.com>
2024-08-16 02:57:43 +0900

    Feat: 로그인/회원가입 반응형, 비밀번호 input visibility icon추가
    Related to:#5

--- apps/user/src/features/session/ui/login-form.tsx
+++ apps/user/src/features/session/ui/login-form.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Button, Input, useToast } from '@jeiltodo/ui/shared';
+import { VisibilityOff, VisibilityOn } from '@jeiltodo/icons';
 import { validateLogIn } from '../model/validation';
 
 export const LoginForm = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
   const [errors, setErrors] = useState<{
     email: string | null;
     password: string | null;
@@ -51,14 +53,20 @@ export const LoginForm = () => {
         break;
     }
   };
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible((prevState) => !prevState);
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div className='w-[640px] flex flex-col gap-y-[24px] mb-[48px]'>
-        <div className='h-[102px] flex flex-col gap-y-[12px]'>
-          <label className='font-pretendard-semibold text-base' htmlFor='email'>
-            아이디
-          </label>
+    <form onSubmit={handleSubmit} className='w-full flex flex-col items-center'>
+      <div className='w-full tablet:max-w-[640px] desktop:w-[640px] flex flex-col gap-y-3 mb-[48px]'>
+        <label className='font-pretendard-semibold text-base' htmlFor='email'>
+          아이디
+        </label>
+        <div className='relative h-20'>
           <Input
+            className='absolute left-0 top-0 w-full'
             name='email'
             onChange={handleChange}
             placeholder='이메일을 입력해주세요'
@@ -66,34 +74,56 @@ export const LoginForm = () => {
             value={email}
           />
           {errors.email ? (
-            <p className='text-error pl-[24px] text-xs -mt-[10px]'>
+            <p className='absolute bottom-2 left-6 text-error text-xs'>
               {errors.email}
             </p>
           ) : null}
         </div>
-        <div className='h-[102px] flex flex-col gap-y-[12px]'>
-          <label
-            className='font-pretendard-semibold text-base'
-            htmlFor='password'
-          >
-            비밀번호
-          </label>
+        <label
+          className='font-pretendard-semibold text-base'
+          htmlFor='password'
+        >
+          비밀번호
+        </label>
+        <div className='relative h-20'>
           <Input
+            className='absolute left-0 top-0 w-full'
             name='password'
             onChange={handleChange}
             placeholder='비밀번호를 입력해주세요'
-            type='password'
+            type={isPasswordVisible ? 'text' : 'password'}
             value={password}
           />
+          <div className='absolute w-[24px] h-[24px] right-[24px] top-4'>
+            <button
+              type='button'
+              onClick={togglePasswordVisibility}
+              className={`absolute left-0 top-0 transition-opacity duration-200 ${
+                isPasswordVisible ? 'opacity-100' : 'opacity-0'
+              }`}
+            >
+              <VisibilityOn className='w-[20px] h-[20px]' />
+            </button>
+
+            <button
+              type='button'
+              onClick={togglePasswordVisibility}
+              className={`absolute left-0 top-0 transition-opacity duration-200 ${
+                !isPasswordVisible ? 'opacity-100' : 'opacity-0'
+              }`}
+            >
+              <VisibilityOff className='w-[20px] h-[20px]' />
+            </button>
+          </div>
           {errors.password ? (
-            <p className='text-error pl-[24px] text-xs -mt-[10px]'>
+            <p className='absolute bottom-2 left-6 text-error text-xs'>
               {errors.password}
             </p>
           ) : null}
         </div>
       </div>
       <Button
-        className='w-full mb-[40px]'
+        className='w-full max-w-[640px] mb-[40px]'
         isDisabled={!email.trim() || !password.trim()}
         variant='primary'
       >
