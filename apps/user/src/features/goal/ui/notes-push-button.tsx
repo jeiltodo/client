b08fbdf322a2de5af553b9dbe251a5d75235c6b6
aajy <aajy.000@gmail.com>
2024-08-18 22:17:15 +0900

    Fix: 노트 모아보기로 이동 및 확인
    Related to:#31

--- apps/user/src/features/goal/ui/notes-push-button.tsx
+++ apps/user/src/features/goal/ui/notes-push-button.tsx
@@ -1,28 +1,24 @@
 import { ArrowRight, Note } from '@jeiltodo/icons';
 import type { Goal } from '@jeiltodo/ui/shared';
-import Link from 'next/link';
 
 export const NotesPushButton = ({ goalData }: { goalData: Goal }) => {
-	return (
-		<Link
-			className='flex justify-between px-6 py-4 rounded-lg bg-blue-100 hover:transform group cursor-pointer'
-			href={{
-				pathname: `/note/[goalid]/list`,
-				query: { goalData: JSON.stringify(goalData) },
-			}}
-			as={`/note/${goalData.id}/list`}
-		>
-			<>
-				<div className='flex gap-x-2 items-center justify-between'>
-					<Note width={24} height={24} />
-					<p>노트 모아보기</p>
-				</div>
-				<ArrowRight
-					width={24}
-					height={24}
-					className='group-hover:transform group-hover:translate-x-1 transition-transform duration-500'
-				/>
-			</>
-		</Link>
-	);
+  return (
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+    <div
+      className='flex justify-between px-6 py-4 rounded-lg bg-blue-100 hover:transform group cursor-pointer'
+      onClick={() => {
+        window.location.href = `/note/list?goalId=${goalData.id}`;
+      }}
+    >
+      <div className='flex gap-x-2 items-center justify-between'>
+        <Note width={24} height={24} />
+        <p>노트 모아보기</p>
+      </div>
+      <ArrowRight
+        width={24}
+        height={24}
+        // className='group-hover:transform group-hover:translate-x-1 transition-transform duration-500'
+      />
+    </div>
+  );
 };
--- apps/user/src/widgets/note/ui/card-list-board.tsx
+++ apps/user/src/widgets/note/ui/card-list-board.tsx
@@ -1,34 +1,35 @@
 'use client';
-import { useParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import { Card, GoalTitleCard } from '../../../features/note';
 import { useGoalNotes } from '../../../entities/note/hooks/useGoalNotes';
 
 export const CardListBoard = () => {
-	const goalTitle = '목표의 제목';
-	const params = useParams();
-	const { data: cardListData, isLoading } = useGoalNotes({
-		goalId: params.goalid,
-		page: 1,
-		limit: 10,
-	});
-	console.log('cardListData: ', cardListData);
+  const goalTitle = '목표의 제목';
+  const searchParams = useSearchParams();
+  const goalId = searchParams.get('goalId');
+  const { data: cardListData, isLoading } = useGoalNotes({
+    goalId: goalId,
+    page: 1,
+    limit: 10,
+  });
+  console.log('cardListData: ', cardListData);
 
-	return (
-		<div className='flex flex-col flex-grow h-full'>
-			<GoalTitleCard title={goalTitle} />
-			<div className='flex-1 flex flex-col'>
-				{!isLoading && cardListData ? (
-					<div className='flex-1 overflow-auto'>
-						{cardListData.data.notes.map((note) => (
-							<Card data={note} key={note.id} />
-						))}
-					</div>
-				) : (
-					<div className='flex-1 flex justify-center items-center'>
-						<p className='text-sm text-slate-500'>아직 등록된 노트가 없어요</p>
-					</div>
-				)}
-			</div>
-		</div>
-	);
+  return (
+    <div className='flex flex-col flex-grow h-full'>
+      <GoalTitleCard title={goalTitle} />
+      <div className='flex-1 flex flex-col'>
+        {!isLoading && cardListData ? (
+          <div className='flex-1 overflow-auto'>
+            {cardListData.data.notes.map((note) => (
+              <Card data={note} key={note.id} />
+            ))}
+          </div>
+        ) : (
+          <div className='flex-1 flex justify-center items-center'>
+            <p className='text-sm text-slate-500'>아직 등록된 노트가 없어요</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
