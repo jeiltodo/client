cc456ed7def13b95603cf09db801f7d5832832b5
aajy <aajy.000@gmail.com>
2024-08-18 21:24:49 +0900

    Fix: 할 일 노트아이콘 -> 노트 작성페이지로 넘어오기(note폴더구조 = path변경)
    Related to:#31

--- apps/user/src/features/todo/ui/todo-list.tsx
+++ apps/user/src/features/todo/ui/todo-list.tsx
@@ -6,9 +6,9 @@ import type { Todo } from '../../../entities/todo';
 import { TodoButtons, TodoModal } from '../../../entities/todo';
 import { TodoContent } from '../../../entities/todo/ui/todo-item';
 import {
-	individualGoalsOptions,
-	userOptions,
-	type Goal,
+  individualGoalsOptions,
+  userOptions,
+  type Goal,
 } from '../../../entities/goal';
 import { ConfirmationModal } from '../../../shared';
 import { useCheckTodo } from '../../../entities/todo/hooks/useCheckTodo';
@@ -16,131 +16,131 @@ import { useDeleteTodo } from '../../../entities/todo/hooks/useDeleteTodo';
 import { NoteDetailSlide } from '../../../widgets/note';
 
 interface Props {
-	todos: (Todo & { goal: Goal })[];
+  todos: (Todo & { goal: Goal })[];
 }
 
 export const TodoList = ({ todos }: Props) => {
-	const [editModalId, setEditModalId] = useState<number | null>(null);
-	const [isNoteSlideModalOpen, setIsNoteSlideModalOpen] =
-		useState<boolean>(false);
-	const [removeModalOpen, setRemoveModalOpen] = useState(false);
-	const [todoToRemove, setTodoToRemove] = useState<number | null>(null);
-	const [noteCreateModalOpen, setNoteCreateModalOpen] = useState(false);
+  const [editModalId, setEditModalId] = useState<number | null>(null);
+  const [isNoteSlideModalOpen, setIsNoteSlideModalOpen] =
+    useState<boolean>(false);
+  const [removeModalOpen, setRemoveModalOpen] = useState(false);
+  const [todoToRemove, setTodoToRemove] = useState<number | null>(null);
+  const [noteCreateModalOpen, setNoteCreateModalOpen] = useState(false);
 
-	const { mutate: checkTodo } = useCheckTodo();
-	const { mutate: deleteTodo } = useDeleteTodo();
-	const { data: userInfo } = useQuery(userOptions());
-	const { data: individualGoals } = useQuery(individualGoalsOptions());
-	const goalsList = individualGoals?.map((item) => ({
-		id: item.id,
-		title: item.title,
-	}));
+  const { mutate: checkTodo } = useCheckTodo();
+  const { mutate: deleteTodo } = useDeleteTodo();
+  const { data: userInfo } = useQuery(userOptions());
+  const { data: individualGoals } = useQuery(individualGoalsOptions());
+  const goalsList = individualGoals?.map((item) => ({
+    id: item.id,
+    title: item.title,
+  }));
 
-	const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-	const handleClickEdit = (id: number) => {
-		setEditModalId(id);
-	};
+  const handleClickEdit = (id: number) => {
+    setEditModalId(id);
+  };
 
-	const handleClickRemove = (id: number) => {
-		setTodoToRemove(id);
-		setRemoveModalOpen(true);
-	};
+  const handleClickRemove = (id: number) => {
+    setTodoToRemove(id);
+    setRemoveModalOpen(true);
+  };
 
-	const handleUrl = (
-		todoId: number,
-		goalId: number,
-		noteId: number | undefined
-	) => {
-		const url = `/note/${noteId || 'new'}?todoId=${todoId}`;
-		window.location.href = url;
-	};
+  const handleUrl = (
+    todoId: number,
+    goalId: number,
+    noteId: number | undefined
+  ) => {
+    const url = `/note/${todoId}?noteId=${noteId || 'new'}`;
+    window.location.href = url;
+  };
 
-	const handleClickNote = (noteId: number | undefined) => {
-		if (noteId) {
-			setEditModalId(noteId);
-			setIsNoteSlideModalOpen(true);
-		} else {
-			setNoteCreateModalOpen(true);
-		}
-	};
+  const handleClickNote = (noteId: number | undefined) => {
+    if (noteId) {
+      setEditModalId(noteId);
+      setIsNoteSlideModalOpen(true);
+    } else {
+      setNoteCreateModalOpen(true);
+    }
+  };
 
-	const handleCheck = (todoId: number) => {
-		checkTodo(todoId, {
-			onSuccess: () => {
-				queryClient.invalidateQueries({
-					predicate: (query) => query.queryKey.includes('todos'),
-				});
-			},
-		});
-	};
+  const handleCheck = (todoId: number) => {
+    checkTodo(todoId, {
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          predicate: (query) => query.queryKey.includes('todos'),
+        });
+      },
+    });
+  };
 
-	const handleRemove = (id: number) => {
-		deleteTodo(id, {
-			onSuccess: () => {
-				queryClient.invalidateQueries({
-					predicate: (query) => query.queryKey.includes('todos'),
-				});
-			},
-		});
-		setRemoveModalOpen(false);
-		setTodoToRemove(null);
-	};
+  const handleRemove = (id: number) => {
+    deleteTodo(id, {
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          predicate: (query) => query.queryKey.includes('todos'),
+        });
+      },
+    });
+    setRemoveModalOpen(false);
+    setTodoToRemove(null);
+  };
 
-	return (
-		<ul className='w-full flex flex-wrap gap-2'>
-			{todos.map(({ id, title, isDone, goal, noteId }) => (
-				<li
-					key={id}
-					className='list-none w-full h-6 flex justify-between group '
-				>
-					<span className='inline-flex gap-2 items-center min-w-[80%]'>
-						<TodoContent
-							key={id}
-							todo={{ id, title, isDone }}
-							onCheck={handleCheck}
-						/>
-					</span>
-					<TodoButtons
-						onClickEdit={() => handleClickEdit(id)}
-						onClickRemove={() => handleClickRemove(id)}
-						onClickNote={() => handleClickNote(noteId)}
-					/>
-					{editModalId === id && (
-						<TodoModal
-							todoCreator={userInfo?.nickname || ''}
-							setTodoModalToggle={() => setEditModalId(null)}
-							goals={goalsList || []}
-							initialTodo={{ id, title, isDone }}
-							initialGoal={goal}
-						/>
-					)}
-					{isNoteSlideModalOpen && (
-						<NoteDetailSlide
-							data={noteId}
-							setToggle={setIsNoteSlideModalOpen}
-						/>
-					)}
-					{removeModalOpen && todoToRemove === id && (
-						<ConfirmationModal
-							setModalToggle={setRemoveModalOpen}
-							onSubmit={() => handleRemove(id)}
-							submitButtonText='삭제'
-						>
-							정말 삭제하시겠습니까?
-						</ConfirmationModal>
-					)}
-					{noteCreateModalOpen && (
-						<ConfirmationModal
-							setModalToggle={setNoteCreateModalOpen}
-							onSubmit={() => handleUrl(id, goal.id, noteId)}
-							submitButtonText='확인'
-						>
-							노트를 작성하시겠습니까?
-						</ConfirmationModal>
-					)}
-				</li>
-			))}
-		</ul>
-	);
+  return (
+    <ul className='w-full flex flex-wrap gap-2'>
+      {todos.map(({ id, title, isDone, goal, noteId }) => (
+        <li
+          key={id}
+          className='list-none w-full h-6 flex justify-between group '
+        >
+          <span className='inline-flex gap-2 items-center min-w-[80%]'>
+            <TodoContent
+              key={id}
+              todo={{ id, title, isDone }}
+              onCheck={handleCheck}
+            />
+          </span>
+          <TodoButtons
+            onClickEdit={() => handleClickEdit(id)}
+            onClickRemove={() => handleClickRemove(id)}
+            onClickNote={() => handleClickNote(noteId)}
+          />
+          {editModalId === id && (
+            <TodoModal
+              todoCreator={userInfo?.nickname || ''}
+              setTodoModalToggle={() => setEditModalId(null)}
+              goals={goalsList || []}
+              initialTodo={{ id, title, isDone }}
+              initialGoal={goal}
+            />
+          )}
+          {isNoteSlideModalOpen && noteId && (
+            <NoteDetailSlide
+              noteId={Number(noteId)}
+              setToggle={setIsNoteSlideModalOpen}
+            />
+          )}
+          {removeModalOpen && todoToRemove === id && (
+            <ConfirmationModal
+              setModalToggle={setRemoveModalOpen}
+              onSubmit={() => handleRemove(id)}
+              submitButtonText='삭제'
+            >
+              정말 삭제하시겠습니까?
+            </ConfirmationModal>
+          )}
+          {noteCreateModalOpen && (
+            <ConfirmationModal
+              setModalToggle={setNoteCreateModalOpen}
+              onSubmit={() => handleUrl(id, goal.id, noteId)}
+              submitButtonText='확인'
+            >
+              노트를 작성하시겠습니까?
+            </ConfirmationModal>
+          )}
+        </li>
+      ))}
+    </ul>
+  );
 };
--- apps/user/src/page/note/editor-page.tsx
+++ apps/user/src/page/note/editor-page.tsx
@@ -2,14 +2,15 @@
 
 import { useEffect, useState } from 'react';
 import {
-	BoardTitle,
-	Button,
-	ButtonGroup,
-	LayoutTitle,
-	TodoTitle,
-	useToast,
+  BoardTitle,
+  Button,
+  ButtonGroup,
+  LayoutTitle,
+  TodoTitle,
+  useToast,
 } from '@jeiltodo/ui/shared';
 import { useParams, useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import { DeleteCircle } from '@jeiltodo/icons';
 import { EditorForm } from '../../features/note';
 import { BaseModal, MINUTES_WITH_MS } from '../../shared';
@@ -17,170 +18,170 @@ import { createNote, patchNote } from '../../entities/note';
 import { useNoteDetail } from '../../entities/note/hooks/useNoteDetail';
 
 export const EditorPage = () => {
-	const [title, setTitle] = useState<string>('');
-	const [content, setContent] = useState<string>('');
-	const [linkUrl] = useState<string>('');
-	const [isLocalSaved, setIsLocalSaved] = useState<boolean>(false);
-	const [isButtonView, setIsButtonView] = useState<boolean>(false);
-	const [isAlert, setIsAlert] = useState<boolean>(false);
-	const { todoid, noteid } = useParams<{
-		todoid: string;
-		noteid: string;
-	}>();
-	//TODO:: 브라우저가 뒤로 가기가 되었을 때 작성중인 어쩌구 팝업 뜨게 하기
-	const showToast = useToast();
-	const router = useRouter();
+  const [title, setTitle] = useState<string>('');
+  const [content, setContent] = useState<string>('');
+  const [linkUrl] = useState<string>('');
+  const [isLocalSaved, setIsLocalSaved] = useState<boolean>(false);
+  const [isButtonView, setIsButtonView] = useState<boolean>(false);
+  const [isAlert, setIsAlert] = useState<boolean>(false);
 
-	if (noteid !== 'new') {
-		const { noteDetail, isLoading } = useNoteDetail(Number(noteid));
-	}
-	//  else {
-	// 	const { todoata, isLoading } = useNoteDatail();
-	// }
+  const searchParams = useSearchParams();
+  const params = useParams();
+  const todoId = params.todoId;
+  const noteId = searchParams.get('noteId');
 
-	const handleLocalSave = () => {
-		const localData = {
-			noteid: noteid,
-			title: title.trim(),
-			content: content.trim(),
-		};
-		if (noteid && title && content) {
-			try {
-				window.localStorage.setItem(`note${noteid}`, JSON.stringify(localData));
-				showToast({
-					message: '임시 저장이 완료되었습니다.',
-					type: 'alert',
-					autoClose: 2000,
-				});
-				setIsLocalSaved(true);
-				setIsButtonView(true);
-			} catch (error) {
-				console.log(error);
-			}
-		}
-	};
+  //TODO:: 브라우저가 뒤로 가기가 되었을 때 작성중인 어쩌구 팝업 뜨게 하기
+  const showToast = useToast();
+  const router = useRouter();
 
-	const getLocalSave = () => {
-		const savedData = localStorage.getItem(`note${noteid}`);
-		const parsedData = savedData
-			? JSON.parse(savedData)
-			: { title: '', content: '' };
-		setTitle(parsedData.title);
-		setContent(parsedData.content);
-	};
+  if (noteId !== 'new') {
+    const { noteDetail, isLoading } = useNoteDetail(Number(noteId));
+  }
+  //TODO:: noteID가 없을 때 목표, 할일 제목 가져오기
 
-	const handleSave = () => {
-		if (!noteid) {
-			onCreateNote();
-		} else {
-			onPatchNote();
-		}
-	};
-	const onCreateNote = async () => {
-		try {
-			const response = await createNote({
-				todoId: todoid,
-				title,
-				content,
-				linkUrl,
-			});
-			console.log('노트가 성공적으로 생성되었습니다:', response);
-			// 필요한 추가 작업을 여기에 작성하세요 (예: 상태 업데이트, UI 변경 등)
-		} catch (error) {
-			console.error('노트 생성 중 오류가 발생했습니다:', error);
-			// 오류 처리 로직을 여기에 추가하세요 (예: 사용자 알림 등)
-		}
-	};
-	const onPatchNote = async () => {
-		try {
-			const response = await patchNote({
-				noteId: noteid,
-				title,
-				content,
-				linkUrl,
-			});
-			if (response.code === 200) setIsAlert(true);
-		} catch (error) {
-			console.error('노트 수정 중 오류가 발생했습니다:', error);
-			// 오류 처리 로직을 여기에 추가하세요 (예: 사용자 알림 등)
-		}
-	};
-	useEffect(() => {
-		if (!isLoading && noteDetail?.data) {
-			setTitle(noteDetail.data.title);
-			setContent(noteDetail.data.content);
-		}
-	}, []);
+  const handleLocalSave = () => {
+    const localData = {
+      noteid: noteId,
+      title: title.trim(),
+      content: content.trim(),
+    };
+    if (noteId && title && content) {
+      try {
+        window.localStorage.setItem(`note${noteId}`, JSON.stringify(localData));
+        showToast({
+          message: '임시 저장이 완료되었습니다.',
+          type: 'alert',
+          autoClose: 2000,
+        });
+        setIsLocalSaved(true);
+        setIsButtonView(true);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
-	useEffect(() => {
-		const showSaveToast = setInterval(() => {
-			handleLocalSave();
-		}, MINUTES_WITH_MS * 5);
+  const getLocalSave = () => {
+    const savedData = localStorage.getItem(`note${noteId}`);
+    const parsedData = savedData
+      ? JSON.parse(savedData)
+      : { title: '', content: '' };
+    setTitle(parsedData.title);
+    setContent(parsedData.content);
+  };
 
-		return () => clearInterval(showSaveToast);
-	}, []);
+  const handleSave = () => {
+    if (!noteId) {
+      onCreateNote();
+    } else {
+      onPatchNote();
+    }
+  };
+  const onCreateNote = async () => {
+    try {
+      const response = await createNote({
+        todoId: todoId,
+        title,
+        content,
+        linkUrl,
+      });
+      console.log('노트가 성공적으로 생성되었습니다:', response);
+      // 필요한 추가 작업을 여기에 작성하세요 (예: 상태 업데이트, UI 변경 등)
+    } catch (error) {
+      console.error('노트 생성 중 오류가 발생했습니다:', error);
+      // 오류 처리 로직을 여기에 추가하세요 (예: 사용자 알림 등)
+    }
+  };
+  const onPatchNote = async () => {
+    try {
+      const response = await patchNote({
+        noteId: noteId,
+        title,
+        content,
+        linkUrl,
+      });
+      if (response.code === 200) setIsAlert(true);
+    } catch (error) {
+      console.error('노트 수정 중 오류가 발생했습니다:', error);
+      // 오류 처리 로직을 여기에 추가하세요 (예: 사용자 알림 등)
+    }
+  };
+  // useEffect(() => {
+  //   if (!isLoading && noteDetail?.data) {
+  //     setTitle(noteDetail.data.title);
+  //     setContent(noteDetail.data.content);
+  //   }
+  // }, []);
 
-	useEffect(() => {
-		if (localStorage.getItem(`note${noteid}`)) {
-			setIsLocalSaved(true);
-			setIsButtonView(true);
-		} else {
-			setIsLocalSaved(false);
-			setIsButtonView(true);
-		}
-	}, [noteid]);
+  useEffect(() => {
+    const showSaveToast = setInterval(() => {
+      handleLocalSave();
+    }, MINUTES_WITH_MS * 5);
 
-	return (
-		<div
-			className='flex flex-col max-w-[792px]'
-			style={{ minHeight: 'calc(100vh - 48px)' }}
-		>
-			<LayoutTitle title={`노트 ${noteid === 'new' ? '작성' : '수정'}`}>
-				<ButtonGroup>
-					<Button
-						className='w-[96px] h-[44px]'
-						onClick={handleLocalSave}
-						variant='text-blue'
-						isDisabled={
-							title.trim().length === 0 || content.trim().length === 0
-						}
-					>
-						임시저장
-					</Button>
-					<Button
-						className='w-[96px] h-[44px]'
-						isDisabled={
-							title.trim().length === 0 || content.trim().length === 0
-						}
-						onClick={handleSave}
-					>
-						저장 완료
-					</Button>
-				</ButtonGroup>
-			</LayoutTitle>
-			{isLocalSaved && isButtonView && (
-				<div className='flex flex-row items-center justify-between bg-[#eff6ff] text-[#3b82f6] rounded-[28px] text-[14px] font-pretendard-medium px-3 pl-6 py-0 shadow-none min-h-[56px] h-[56px] mb-6'>
-					<div className='flex flex-row items-center gap-4'>
-						<span
-							onClick={() => setIsButtonView(false)}
-							className='cursor-pointer'
-						>
-							<DeleteCircle width={24} height={24} />
-						</span>
-						<p>임시 저장된 노트가 있어요. 저장된 노트를 불러오시겠어요?</p>
-					</div>
-					<Button
-						variant='rounded-outline-blue'
-						className='block px-3 !text-sm !h-[36px] leading-5'
-						onClick={getLocalSave}
-					>
-						불러오기
-					</Button>
-				</div>
-			)}
-			{/* {!isLoading &&
+    return () => clearInterval(showSaveToast);
+  }, []);
+
+  useEffect(() => {
+    if (localStorage.getItem(`note${noteId}`)) {
+      setIsLocalSaved(true);
+      setIsButtonView(true);
+    } else {
+      setIsLocalSaved(false);
+      setIsButtonView(true);
+    }
+  }, [noteId]);
+
+  return (
+    <div
+      className='flex flex-col max-w-[792px]'
+      style={{ minHeight: 'calc(100vh - 48px)' }}
+    >
+      <LayoutTitle title={`노트 ${noteId === 'new' ? '작성' : '수정'}`}>
+        <ButtonGroup>
+          <Button
+            className='w-[96px] h-[44px]'
+            onClick={handleLocalSave}
+            variant='text-blue'
+            isDisabled={
+              title.trim().length === 0 || content.trim().length === 0
+            }
+          >
+            임시저장
+          </Button>
+          <Button
+            className='w-[96px] h-[44px]'
+            isDisabled={
+              title.trim().length === 0 || content.trim().length === 0
+            }
+            onClick={handleSave}
+          >
+            저장 완료
+          </Button>
+        </ButtonGroup>
+      </LayoutTitle>
+      {isLocalSaved && isButtonView && (
+        <div className='flex flex-row items-center justify-between bg-[#eff6ff] text-[#3b82f6] rounded-[28px] text-[14px] font-pretendard-medium px-3 pl-6 py-0 shadow-none min-h-[56px] h-[56px] mb-6'>
+          <div className='flex flex-row items-center gap-4'>
+            <span
+              onClick={() => setIsButtonView(false)}
+              className='cursor-pointer'
+            >
+              <DeleteCircle width={24} height={24} />
+            </span>
+            <p>임시 저장된 노트가 있어요. 저장된 노트를 불러오시겠어요?</p>
+          </div>
+          <Button
+            variant='rounded-outline-blue'
+            className='block px-3 !text-sm !h-[36px] leading-5'
+            onClick={getLocalSave}
+          >
+            불러오기
+          </Button>
+        </div>
+      )}
+      {/* {!isLoading &&
 				noteDetail(
-      */}
+      
 			{
 				true && (
 					<>
@@ -188,7 +189,7 @@ export const EditorPage = () => {
 							className='mb-[12px]'
 							icon='flag'
 							iconSize={24}
-							title={noteDetail.data.todo.title}
+							title={noteDetail.data.todo.title ||'목표 '}
 						/>
 						<TodoTitle
 							className='mb-[24px]'
@@ -197,29 +198,31 @@ export const EditorPage = () => {
 					</>
 				)
 				// )
+				
 			}
-			<EditorForm
-				content={content}
-				setContent={setContent}
-				setTitle={setTitle}
-				title={title}
-			/>
-			{isAlert && (
-				<div className='w-[300px]'>
-					<BaseModal setToggle={setIsAlert}>
-						<div className='flex flex-col gap-3 items-center'>
-							<p>저장 되었습니다!</p>
-							<Button
-								isDisabled={!title}
-								className='w-[120px] mt-10 h-12'
-								onClick={() => router.back()}
-							>
-								확인
-							</Button>
-						</div>
-					</BaseModal>
-				</div>
-			)}
-		</div>
-	);
+				*/}
+      <EditorForm
+        content={content}
+        setContent={setContent}
+        setTitle={setTitle}
+        title={title}
+      />
+      {isAlert && (
+        <div className='w-[300px]'>
+          <BaseModal setToggle={setIsAlert}>
+            <div className='flex flex-col gap-3 items-center'>
+              <p>저장 되었습니다!</p>
+              <Button
+                isDisabled={!title}
+                className='w-[120px] mt-10 h-12'
+                onClick={() => router.back()}
+              >
+                확인
+              </Button>
+            </div>
+          </BaseModal>
+        </div>
+      )}
+    </div>
+  );
 };
