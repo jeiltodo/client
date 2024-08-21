df2481d242623596ff397402ccce7a8bb1e9430b
dragonite-lee <ross1222@naver.com>
2024-08-21 11:29:04 +0900

    #93 fix: mobile sidebar 수정 작업

--- apps/user/src/app/(dashboard)/page.tsx
+++ apps/user/src/app/(dashboard)/page.tsx
@@ -2,7 +2,7 @@ import { UserDashboardPage } from '../../page/user/user-dashboard-page';
 
 export default function Page() {
   return (
-    <main className="pt-4 common-layout bg-slate-100">
+    <main className="common-layout bg-slate-100">
       <UserDashboardPage />
     </main>
   );
