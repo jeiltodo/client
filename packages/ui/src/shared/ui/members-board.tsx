eee1c303b83dc38c77dc1492ec7c6ee14ac2812b
aajy <aajy.000@gmail.com>
2024-08-26 02:10:17 +0900

    Fix: 공용 컴포넌트에 어드민 그룹상세페이지를 위한 분기처리용 boolean데이터 추가
    Related to:#123

--- packages/ui/src/shared/ui/members-board.tsx
+++ packages/ui/src/shared/ui/members-board.tsx
@@ -8,83 +8,83 @@ import { BgGroupAvatar, GroupFill } from '@jeiltodo/icons';
 import { MemberList, MembersManageButtons } from '../../features';
 
 interface Props {
-  group: GroupWithMembers;
-  userId?: number;
-  onChangeLeader: (id: number) => void;
-  onRemoveMember: (id: number) => void;
+	group: GroupWithMembers;
+	userId?: number;
+	onChangeLeader: (id: number) => void;
+	onRemoveMember: (id: number) => void;
 }
 
 export const MembersBorad = ({
-  group,
-  userId,
-  onChangeLeader,
-  onRemoveMember,
+	group,
+	userId,
+	onChangeLeader,
+	onRemoveMember,
 }: Props) => {
-  const { mode } = useBoardContext();
+	const { mode } = useBoardContext();
 
-  const leaderId = group.members.find((member) => member.isLeader)!.id;
-  const isUserALeader = leaderId === userId;
-  const [newLeaderId, setNewLeaderId] = useState<number>(leaderId);
-  const [deletedId, setDeletedId] = useState<number | null>(null);
+	const leaderId = group.members.find((member) => member.isLeader)!.id;
+	const isUserALeader = leaderId === userId;
+	const [newLeaderId, setNewLeaderId] = useState<number>(leaderId);
+	const [deletedId, setDeletedId] = useState<number | null>(null);
 
-  const sortedMembers = group.members
-    .map((member) => ({ id: member.id, rank: member.contributionRank }))
-    .sort((a, b) => a.rank - b.rank);
-  const lowestRankNum = sortedMembers.pop()!.rank;
+	const sortedMembers = group.members
+		.map((member) => ({ id: member.id, rank: member.contributionRank }))
+		.sort((a, b) => a.rank - b.rank);
+	const lowestRankNum = sortedMembers.pop()!.rank;
 
-  const formattedMembers = group.members
-    .map((member) => ({
-      ...member,
-      contributionRank: getFormattedRanks(
-        member.id,
-        sortedMembers,
-        lowestRankNum
-      ),
-    }))
-    .filter((member) => member.id !== deletedId);
+	const formattedMembers = group.members
+		.map((member) => ({
+			...member,
+			contributionRank: getFormattedRanks(
+				member.id,
+				sortedMembers,
+				lowestRankNum
+			),
+		}))
+		.filter((member) => member.id !== deletedId);
 
-  const handleClientChange = (newLeaderId: number) => {
-    setNewLeaderId(newLeaderId);
-  };
+	const handleClientChange = (newLeaderId: number) => {
+		setNewLeaderId(newLeaderId);
+	};
 
-  const handleClientRemove = (memberId: number) => {
-    setDeletedId(memberId);
-  };
+	const handleClientRemove = (memberId: number) => {
+		setDeletedId(memberId);
+	};
 
-  const handleSave = () => {
-    mode === 'change-leader'
-      ? onChangeLeader(newLeaderId)
-      : deletedId && onRemoveMember(deletedId);
-  };
+	const handleSave = () => {
+		mode === 'change-leader'
+			? onChangeLeader(newLeaderId)
+			: deletedId && onRemoveMember(deletedId);
+	};
 
-  return (
-    <div className='w-full px-6 py-4 rounded-lg bg-orange-500 h-[280px] desktop:h-[364px] overflow-hidden'>
-      <div className='w-full h-full relative '>
-        <div className='flex justify-between items-center mb-6'>
-          <div className='flex gap-4 items-center'>
-            <div className='hidden tablet:flex gap-2 items-center'>
-              <GroupFill width={40} height={40} />
-              <span className='font-semibold text-lg text-white'>구성원</span>
-            </div>
-            <div className='bg-white opacity-50 rounded-md w-fit h-fit px-1 py-0 flex justify-center items-center bottom-1'>
-              <span className='text-slate-800'>{group.members.length}</span>
-              <span className='text-groupColor-500'>/10</span>
-            </div>
-          </div>
+	return (
+		<div className='w-full px-6 py-4 rounded-lg bg-orange-500 h-[280px] overflow-hidden'>
+			<div className='w-full h-full relative '>
+				<div className='flex justify-between items-center mb-6'>
+					<div className='flex gap-4 items-center'>
+						<div className='hidden tablet:flex gap-2 items-center'>
+							<GroupFill width={40} height={40} />
+							<span className='font-semibold text-lg text-white'>구성원</span>
+						</div>
+						<div className='bg-white opacity-50 rounded-md w-fit h-fit px-1 py-0 flex justify-center items-center bottom-1'>
+							<span className='text-slate-800'>{group.members.length}</span>
+							<span className='text-groupColor-500'>/10</span>
+						</div>
+					</div>
 
-          {isUserALeader && <MembersManageButtons onSave={handleSave} />}
-        </div>
-        <MemberList
-          members={formattedMembers}
-          onClientChangeLeader={handleClientChange}
-          onClientRemoveMember={handleClientRemove}
-        />
-        <BgGroupAvatar
-          // width={164}
-          // height={164}
-          className='block w-[120px] h-[120px] right-[-12px] bottom-[-16px] desktop:w-[184px] desktop:h-[184px] absolute z-10 desktop:right-[-32px] desktop:bottom-[-24px]'
-        />
-      </div>
-    </div>
-  );
+					{isUserALeader && <MembersManageButtons onSave={handleSave} />}
+				</div>
+				<MemberList
+					members={formattedMembers}
+					onClientChangeLeader={handleClientChange}
+					onClientRemoveMember={handleClientRemove}
+				/>
+				<BgGroupAvatar
+					width={164}
+					height={164}
+					className='block w-[120px] h-[120px] right-[-12px] bottom-[-16px] desktop:w-[184px] desktop:h-[184px] absolute z-10 desktop:right-[-32px] desktop:bottom-[-24px]'
+				/>
+			</div>
+		</div>
+	);
 };
