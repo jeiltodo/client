585bdf37fe01234e3ad85c11f8f56f8de6865621
aajy <aajy.000@gmail.com>
2024-08-26 02:01:58 +0900

    Fix: Field 컴포넌트에서  p태그 안에 div올 수 없다는 에러 해결

--- packages/ui/src/shared/ui/field.tsx
+++ packages/ui/src/shared/ui/field.tsx
@@ -1,16 +1,16 @@
 import { PropsWithChildren } from 'react';
 
 interface Props extends PropsWithChildren {
-  label: string;
+	label: string;
 }
 
 export const Field = ({ label, children }: Props) => {
-  return (
-    <div className='flex flex-wrap w-full gap-1'>
-      <span className='inline-block w-full text-sm text-slate-800 font-light opacity-50'>
-        {label}
-      </span>
-      <p className='text-lg text-slate-800 font-semibold'>{children}</p>
-    </div>
-  );
+	return (
+		<div className='flex flex-wrap w-full gap-1'>
+			<span className='inline-block w-full text-sm text-slate-800 font-light opacity-50'>
+				{label}
+			</span>
+			<div className='text-lg text-slate-800 font-semibold'>{children}</div>
+		</div>
+	);
 };
