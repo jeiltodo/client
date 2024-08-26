04c1eed78aaabf5e76b1a5fa4cb3bdb3ee27de89
dragonite-lee <ross1222@naver.com>
2024-08-26 09:27:00 +0900

    #122 fix: dropdown value 수정

--- apps/admin/src/shared/ui/@x/table-toolbar/index.tsx
+++ apps/admin/src/shared/ui/@x/table-toolbar/index.tsx
@@ -40,7 +40,7 @@ export function TableToolBar({
         <Dropdown.Toggle />
         <Dropdown.Menu>
           <Dropdown.Item value='10'>10개씩 보기</Dropdown.Item>
-          <Dropdown.Item value='10'>20개씩 보기</Dropdown.Item>
+          <Dropdown.Item value='20'>20개씩 보기</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
     </div>
