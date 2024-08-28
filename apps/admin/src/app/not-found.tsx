eeefb0bd593f5ddd94a7ca664058e53180fbb23f
dragonite-lee <ross1222@naver.com>
2024-08-28 11:03:29 +0900

    #122 fix: content수정

--- apps/admin/src/app/not-found.tsx
+++ apps/admin/src/app/not-found.tsx
@@ -12,7 +12,7 @@ const NotFoundPage = () => {
         <p className='text-xl text-gray-600 mb-8'>Oops! Page not found.</p>
         <div className='space-y-4'>
           <button
-            onClick={() => router.push('/admin')}
+            onClick={() => router.push('/')}
             className='px-6 py-3 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-300'
           >
             Back to Admin Dashboard
